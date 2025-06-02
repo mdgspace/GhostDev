c9e66c16748ab2587760924333f9f52a3d03dd4d
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-06-03 01:04:15 +0530

    feat: Update UI, add LLM generation logic, add LLM settings

--- pipeline/test_client.py
+++ pipeline/test_client.py
@@ -1,23 +1,30 @@
-import os
 import gradio as gr
+from main import exctract_code_from_selected_files, generate_code_from_llm
 
-os.chdir(os.getcwd())
-
-selected_files = []
+file_to_code_json = {}
 
 def save_files(files):
-    global selected_files
-    selected_files = files
-
-    print(selected_files)
-
+    global file_to_code_json
+    file_to_code_json = exctract_code_from_selected_files(files)
     return (f"{len(files)} file(s) saved!")
 
-with gr.Blocks() as test_client:
-    file_selector = gr.File(label="Select Files", file_types=["file"], file_count="multiple")
-    save_button = gr.Button("Save")
-    output = gr.Textbox(label="Status")
+def process_user_input_code(code):
+    global file_to_code_json
+    return generate_code_from_llm(code, file_to_code_json)
 
-    save_button.click(save_files, inputs=file_selector, outputs=output)
+
+with gr.Blocks() as test_client:
+    file_selector = gr.File(label="Select Files", file_types=["file"], file_count="multiple", height=165)
+    file_save_button = gr.Button("Save")
+    output = gr.Textbox(label="Status")
+    
+    with gr.Row():
+        code_input = gr.Textbox(label="Write Code Here", lines=25, placeholder="Enter your code...")
+        output_display = gr.Textbox(label="Output Display", lines=25, interactive=False)
+    
+    code_process_button = gr.Button("Process Code")
+
+    file_save_button.click(save_files, inputs=file_selector, outputs=output)
+    code_process_button.click(process_user_input_code, inputs=code_input, outputs=output_display)
 
 test_client.launch()
