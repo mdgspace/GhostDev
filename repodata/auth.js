9f21661abeda32a478fe303f75725c390e7444db
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-06-04 02:10:50 +0530

    feat: access to client

---
+++ repodata/auth.js
@@ -0,0 +1,43 @@
+
+const getCodeUrl = "https://github.com/login/device/code"
+
+const fs = require('fs');
+function getClientId() {
+  const data = fs.readFileSync('ghostdev/client_id.txt');
+  return data.toString();
+}
+
+async function getCode(){
+    const client_id = getClientId();
+    const headers = {'Content-Type':'application/json',
+        'Accept':'application/json',
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'
+    }
+    try {
+        const response = await fetch(getCodeUrl, {
+            statusCode: 200, 
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify({"client_id": client_id, "scope": "repo read:user"})
+        });
+        if (response.ok) {
+            const result = await response.json();
+            const device_code = result.device_code;
+            const user_code = result.user_code;
+            return (user_code)
+            console.log(result)
+        } else {
+            const errorData = response;
+            console.error("error:", errorData);
+        }
+    } catch (error) {
+        console.error("Error", error);
+    }
+};
+async function main() {
+  const user_code = await getCode();
+  console.log("Go to this site: https://github.com/login/device/ and enter the code:",user_code); 
+}
+
+main();
