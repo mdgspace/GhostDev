f9ff2d0e8f2f5ccb209d1871aa67f3deaf6ccd48
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-06-22 18:29:13 +0530

    chore: trials

---
+++ repodata/commit.py
@@ -0,0 +1,13 @@
+import os
+from git import Repo
+
+cwd = os.getcwd()
+
+repo_path = cwd
+repo = Repo(repo_path)
+
+diff_cached = repo.index.diff("HEAD")
+
+for diff in diff_cached:
+    print(f"Staged change in: {diff.a_path}")
+    print(diff.diff.decode())
