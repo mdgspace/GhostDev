66b2e7115da408c47dabae224ec54b0b61b8d1a0
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-06-22 19:07:30 +0530

    chore: trials

---
+++ repodata/diff.py
@@ -0,0 +1,18 @@
+import os
+from git import Repo
+#from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+cwd = os.getcwd()
+
+repo_path = cwd
+repo = Repo(repo_path)
+
+class GitIndexChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if ".git/index" in event.src_path:
+            print("\n:arrows_counterclockwise: Detected staging change via git add!")
+            diff_cached = repo.git.diff("--cached")
+            print(diff_cached)
+
+
+
